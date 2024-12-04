c90081bd68b9c2c7a18966dd971b61077121c8f8
Jane <apiwatsupaphol@gmail.com>
2024-12-05 02:29:26 +0700

    feat: added profile page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,62 @@
-import Image from "next/image";
+"use client";
+import { Button, Image, Link } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+    <main className="bg-Grey-900 h-screen py-32">
+      <section className="sm:max-w-md max-w-sm mx-auto sm:p-8 p-6 rounded-xl bg-Grey-800">
+        <div className="flex flex-col w-full justify-center items-center gap-6">
+          <div>
             <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
+              src={"/images/chatjane.jpg"}
+              alt="Avatar"
+              width={80}
+              height={80}
+              radius="full"
+              className="object-cover"
             />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          </div>
+          <div className="flex flex-col gap-2 items-center">
+            <div className="text-2xl text-white font-medium">
+              Apiwat Supaphol
+            </div>
+            <div className="font-semibold text-Green">Bangkok, Thailand</div>
+          </div>
+          <div className="flex flex-col gap-4 items-center w-full">
+            <div className="text-white">"Front-end developer"</div>
+            <div className="grid grid-cols-1 w-full gap-4">
+              <Button
+                size="lg"
+                as={Link}
+                href="https://github.com/Janeninie"
+                isExternal
+                className="bg-Grey-700 text-white hover:bg-Green hover:text-inherit"
+              >
+                Github
+              </Button>
+              <Button
+                size="lg"
+                as={Link}
+                href="https://www.linkedin.com/in/apiwat-supaphol-3b0953324/"
+                isExternal
+                className="bg-Grey-700 text-white hover:bg-Green hover:text-inherit"
+              >
+                LinkedIn
+              </Button>
+
+              <Button
+                size="lg"
+                as={Link}
+                href="https://www.instagram.com/apiw.jane/"
+                isExternal
+                className="bg-Grey-700 text-white hover:bg-Green hover:text-inherit"
+              >
+                Instagram
+              </Button>
+            </div>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </section>
+    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,3 +1,4 @@
+import { nextui } from "@nextui-org/react";
 import type { Config } from "tailwindcss";
 
 export default {
@@ -5,14 +6,21 @@ export default {
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        White: "hsl(0, 0%, 100%)",
+        Green: "hsl(75, 94%, 57%)",
+        Grey: {
+          700: "hsl(0, 0%, 20%)",
+          800: "hsl(0, 0%, 12%)",
+          900: "hsl(0, 0%, 8%)",
+        },
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui()],
 } satisfies Config;
